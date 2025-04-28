2087668466fdff8e1c980f3e654ae3ab0e910c60
yevpy <eugene2piskun@gmail.com>
2025-04-28 18:58:33 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12-slim
+LABEL maintainer="eugene2piskun@gmail.com"
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+WORKDIR /app
+
+COPY app/ /app/
+
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.environ.get("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY not found.")
+
+    url = "https://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": api_key,
+        "q": "Paris",
+        "aqi": "no"
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    city = data["location"]["name"]
+    temperature = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(f"Current weather in {city}: {temperature}°C, {condition}")
 
 
 if __name__ == "__main__":
